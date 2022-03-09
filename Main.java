163848ede37436c002d503aa1ea13ab8eca21537
Stevenijames <ijamesproductions1@gmail.com>
2022-03-08 22:59:13 -0700

    Java Learning-Variables, Operators, Looping, Arrays

---
+++ Main.java
@@ -0,0 +1,60 @@
+package com.company;
+
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Primitive Types
+        byte age = 30;
+        byte temperature = 20;
+        //long can store in the billions, add L to end of number
+        long viewsCount = 3_123_456_789L;
+        float price = 10.99F;
+        char letter = 'A';
+        boolean isEligible = false;
+        Date now = new Date();
+        System.out.println(now);
+
+        System.out.println(age);
+        System.out.println(temperature);
+
+
+//        int myValue = 10000;
+//
+//        int myMinIntValue = Integer.MIN_VALUE;
+//        int myMaxIntValue = Integer.MAX_VALUE;
+//        System.out.println("Inter Minimum Value = " + myMinIntValue);
+//        System.out.println("Inter Maximum Value = " + myMaxIntValue);
+//        System.out.println("Busted MAX value = " + (myMaxIntValue + 1));
+//        System.out.println("Busted MIN value = " + (myMinIntValue - 1));
+//
+//        int myMaxIntTest = 2147483647;
+//
+//        byte myMinByteValue = Byte.MIN_VALUE;
+//        byte myMaxByteValue = Byte.MAX_VALUE;
+//        System.out.println("Byte Minimum Value = " + myMinByteValue);
+//        System.out.println("Byte Maximum Value = " + myMaxByteValue);
+//
+//        short myMinShortValue = Short.MIN_VALUE;
+//        short myMaxShortValue = Short.MAX_VALUE;
+//        System.out.println("Short Minimum Value = " + myMinShortValue);
+//        System.out.println("Short Maximum Value = " + myMaxShortValue);
+//
+//        long myLongValue = 100L;
+//        long myMinLongValue = Long.MIN_VALUE;
+//        long myMaxLongValue = Long.MAX_VALUE;
+//        System.out.println("Long Minimum Value = " + myMinLongValue);
+//        System.out.println("Long Maximum Value = " + myMaxLongValue);
+//        long bigLongLiteralValue = 2147483647234L;
+//        System.out.println(bigLongLiteralValue);
+//
+//        short bigShortLiteralValue = 32767;
+//
+//        int myTotal = (myMinIntValue / 2);
+//
+//        byte myNewByteValue = (byte) (myMinByteValue / 2);
+//
+//        short myNewShortValue = (short) (myMinShortValue / 2);
+    }
+}
---
+++ helloWorld.java
@@ -0,0 +1,60 @@
+package com.company;
+
+public class helloWorld {
+    public static void main(String[] args) {
+
+//        int x = -5;
+//
+//        char y = 'y';
+//
+//        String s = "Hello World";
+
+//        System.out.println(x);
+//        System.out.println(y);
+//        System.out.println(s);
+//        System.out.println(4 % 2);
+//        System.out.println(x++);
+//        System.out.println(++x);
+//        int totalSugarInGlass = 0;
+//
+//        int sugar = 2;
+//
+//        int maxSugar = 10;
+
+
+//        arrays represent objects
+
+//        if (sugar <= maxSugar) {
+//            totalSugarInGlass += sugar;
+//        } else {
+//            System.out.println("We only Allow 10 tsp. of sugar per glass");
+//        }
+//        System.out.println("I run no matter what");
+
+//        for (int i = 1; i <= 10; i++){
+//            System.out.println(i);
+//        }
+//        for (; totalSugarInGlass < maxSugar; totalSugarInGlass += sugar){
+//            System.out.println("Total Sugar in Glass: " + totalSugarInGlass);
+//        }
+//        System.out.println("I print out once loop finishes");
+
+//        String s = "Hello";
+
+//        int[] numbers = new int[5];
+
+//        int[] numbers = new int[] {1,2,3,4,5};
+//        numbers = new int[] {1,2,3,4,5,6,7};
+//
+//        int count = 1;
+//        for(int i = 0; i < numbers.length; i++, count++){
+//            numbers[i] = count;
+//    }
+//        for (int i = 0; i < numbers.length; i++)
+//        System.out.println(numbers[i]);
+//        toString() exists on all objects
+
+    }
+}
+
+
